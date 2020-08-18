0cc37cfa484a2a365cc28c367d7851e38368d38b
Maloochow <luyao.mly@gmail.com>
2020-08-18 09:41:21 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
+    def display_artist(song)
+        if song.artist
+            # <a href="artist_path(artist)">
+            link_to song.artist_name, artist_path(song.artist)
+        else
+            # <a href="edit_song_path(self)">Add Artist</a>
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 
 end
